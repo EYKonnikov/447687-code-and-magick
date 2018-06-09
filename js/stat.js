d2ee20e895eb9052463c826f745fc3d043a2f741
Evgeny Konnikov-Lehmann <ykonnikov@yandex.com>
2018-06-09 21:43:43 +0200

    update v0.1

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,11 @@
 'use strict';
+var CLOUD = {
+  width: 420,
+  height: 270,
+  x: 100,
+  y: 10
+};
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 20;
 var BAR_WIDTH = 40;
@@ -14,29 +16,26 @@ var BAR_Y = 100;
 // функция для отрисовки "облака" с тенью
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, CLOUD.width, CLOUD.height);
 };
 
 // функция для вывода статистики
 window.renderStatistics = function (ctx, names, times) {
   //  рисуем облако и тень
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD.x + GAP, CLOUD.y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD.x, CLOUD.y, '#fff');
 
   //  выводим сообщение о победе
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP, CLOUD_Y + GAP + FONT_GAP * 2);
+  ctx.fillText('Ура вы победили!', CLOUD.x + FONT_GAP, CLOUD.y + GAP + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD.x + FONT_GAP, CLOUD.y + GAP + FONT_GAP * 2);
 
   //  ищем максимальное время в массиве для рассчета высоты столбцов гистограммы
   var maxTime = times[0];
   for (var i = 1; i < times.length; i++) {
-    if (times[i] > maxTime) {
-      maxTime = times[i];
-    }
+      maxTime = Math.max(times[i], times[0]);
   }
-
   // пропорция для рассчета высоты:
   // 300s = 150px (максимальное время = максимальная высота)
   // 1px = 300s/150px
@@ -56,13 +55,13 @@ window.renderStatistics = function (ctx, names, times) {
     }
     // рисуем столбцы
     ctx.fillStyle = columnColor;
-    ctx.fillRect(CLOUD_X + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), (BAR_MAX_HEIGHT - columnHeight) + BAR_Y, BAR_WIDTH, columnHeight);
+    ctx.fillRect(CLOUD.x + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), (BAR_MAX_HEIGHT - columnHeight) + BAR_Y, BAR_WIDTH, columnHeight);
 
     // делаем подписи: результаты над колонками и имена под колонками
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), (BAR_MAX_HEIGHT - columnHeight) + BAR_Y - FONT_GAP);
-    ctx.fillText(names[i], CLOUD_X + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), BAR_Y + BAR_MAX_HEIGHT + FONT_GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD.x + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), (BAR_MAX_HEIGHT - columnHeight) + BAR_Y - FONT_GAP);
+    ctx.fillText(names[i], CLOUD.x + FONT_GAP * 2 + i * (BARS_GAP + BAR_WIDTH), BAR_Y + BAR_MAX_HEIGHT + FONT_GAP);
   }
 
 };
